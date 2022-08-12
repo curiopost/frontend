597c43369d2175efbedd3b13b1b9b1029be894b4
Shaheer <shaheer.myhome@gmail.com>
2022-08-12 18:53:01 +0530

    fix some bugs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Feeds from "./loggedInHomePage"
 import Home from "./Home"
-import Redirector from "./functions/redirector"
 import Login from "./login"
 import Logout from "./logout";
 import Register from "./Register"
