d1c5568ee50cc5f0790159302ea5ea5ba29a01e5
Shaheer <shaheer.myhome@gmail.com>
2022-08-12 23:24:47 +0530

    fixed mor bugs

--- src/about.js
+++ src/about.js
@@ -9,7 +9,7 @@ return (
     <div>
         <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
         <div className="container-fluid">
-            <Link to="/" className="navbar-brand"><img src="/logo.png" alt="logo" width="30" height="30" className="d-inline-block align-text-top"/> Curiopost</Link>
+            <Link to="/" className="navbar-brand"><img src={process.env.PUBLIC_URL+"/logo.png"} alt="logo" width="30" height="30" className="d-inline-block align-text-top"/> Curiopost</Link>
 
             <button className="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-expanded="false" aria-controls="#navbarNav"  aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
