e4ab0cc15c90f815adb7b3caf5c77d227aff31f4
Shaheer Ahamed <92243459+spicybirsge@users.noreply.github.com>
2022-09-28 20:18:54 +0530

    added service worker

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
  
@@ -9,3 +9,5 @@ root.render(
   
 );
 
+
+serviceWorkerRegistration.unregister();
