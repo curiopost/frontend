215507f8a7ef72be920c52407b4d6c20bfc0896a
Shaheer Ahamed <92243459+spicybirsge@users.noreply.github.com>
2022-11-15 16:40:27 +0530

    added re captcha to prevent spam

--- src/loginPage.js
+++ src/loginPage.js
@@ -4,10 +4,12 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import "./home.css"
 import urls from './variables/urls'
+import RECAPTCHA from "react-google-recaptcha"
 
-export default function loginPage() {
+
+export default function LoginPage() {
     document.title = "Login / Curiopost"
-
+    const captchaRef = React.useRef(null)
     const buffer = (element, text) => {
 
         element.disabled = true 
@@ -23,8 +25,15 @@ export default function loginPage() {
 
     }
 
-    const login = async() => {
-
+    const login = async(e) => {
+        e.preventDefault();
+        const captcha_token = captchaRef.current.getValue();
+        if(!captcha_token) {
+  return document.getElementById('bot-fail').innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
+  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+  <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+  </svg> confirm that your not a robot`
+        }
         buffer(document.getElementById("loginbtn"), "Logging you in..")
         const email =  document.getElementById("email").value
         const password = document.getElementById("password").value
@@ -34,7 +43,7 @@ export default function loginPage() {
             return toast.error("Please fill up all  the fields!")
         }
 
-        const resp = await fetch(urls.backend+'/api/auth/login', {
+        const resp = await fetch(urls.backend+`/api/auth/login?r_key=${captcha_token}`, {
             method: 'POST',
             headers: {
                 "Content-Type": "application/json"
@@ -48,6 +57,7 @@ export default function loginPage() {
         const data = await resp.json()
 
         if(!data.success)  {
+            captchaRef.current.reset();
             unbuffer(document.getElementById("loginbtn"), "Login")
            return toast.error(data.message)
         } else {
@@ -98,7 +108,9 @@ export default function loginPage() {
          
         <div className="col-lg-6 offset-lg-3"  style={{"marginTop": "10%", }}>
         <h4 className="text-muted">Welcome back, we're so excited to see you again!</h4>
+        <form onSubmit={login}>
             <div className="input-group mb-4 p-1">
+               
             <span className="input-group-text" id="b1"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
   <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/>
 </svg></span>
@@ -115,12 +127,29 @@ export default function loginPage() {
   <path d="M4.81 1.37A6.5 6.5 0 0 1 14.56 7a.5.5 0 1 1-1 0 5.5 5.5 0 0 0-8.25-4.765.5.5 0 0 1-.5-.865Zm-.89 1.257a.5.5 0 0 1 .04.706A5.478 5.478 0 0 0 2.56 7a.5.5 0 0 1-1 0c0-1.664.626-3.184 1.655-4.333a.5.5 0 0 1 .706-.04ZM1.915 8.02a.5.5 0 0 1 .346.616l-.779 2.767a.5.5 0 1 1-.962-.27l.778-2.767a.5.5 0 0 1 .617-.346Zm12.15.481a.5.5 0 0 1 .49.51c-.03 1.499-.161 3.025-.727 4.533l-.07.187a.5.5 0 0 1-.936-.351l.07-.187c.506-1.35.634-2.74.663-4.202a.5.5 0 0 1 .51-.49Z"/>
 </svg></span>
 <input type="password" className="form-control"  placeholder="Password"  aria-label="Email" aria-describedby="b2" id="password"/>
+           
+           
             </div>
-      
-            <button className=" btn btn-success w-100 text-center" id="loginbtn" style={{"marginBottom": "2%", }} onClick={login}>Login</button>      
-            <Link to="/register" className="float-start text-muted">Don't have an account? Create One!</Link>
-
+            <RECAPTCHA
+sitekey={"6LcYoQojAAAAADYeEE1m7AQFQAgix5lP-g5rPehU"}
+ref={captchaRef}
+render="explicit"
+required
+/>
+<p className="text-danger" id="bot-fail" style={{"marginBottom": "0%"}}></p>
+            <button className=" btn btn-success w-100 text-center" id="loginbtn" style={{"marginBottom": "2%", marginTop: "2%"}} >Login</button>      
+            </form>
+       {/* soonTM <div className="text-center"style={{"marginBottom": "2%", }}>
+            <Link to="/reset_password" className="text-center text-success text-decoration-none" >Forgot your password?</Link>
+                </div>*/}
             </div>
+            
+            <div className="text-center">
+            <p  className="text-center text-muted " >Don't have an account? <Link to="/register" className="text-success text-decoration-none">Create One!</Link></p>
+           
+            
+            </div>
+            
         </div>
         </div>
     )
--- src/registerPage.js
+++ src/registerPage.js
@@ -4,11 +4,12 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import "./home.css"
 import urls from './variables/urls'
-
+import RECAPTCHA from "react-google-recaptcha"
+import { useRef } from "react";
 
 export default function RegisterPage() {
     document.title = `Register / Curiopost`
-
+    const captchaRef = useRef(null)
     const buffer = (element, text) => {
 
         element.disabled = true 
@@ -24,8 +25,17 @@ export default function RegisterPage() {
 
     }
 
-    const register = async() => {
-        buffer(document.getElementById("registerbtn"), "Registering...")
+    const register = async(e) => {
+        e.preventDefault();
+        
+        const captcha_token = captchaRef.current.getValue();
+      if(!captcha_token) {
+return document.getElementById('bot-fail').innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
+<path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+<path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+</svg> confirm that your not a robot`
+      }
+      buffer(document.getElementById("registerbtn"), "Registering...")
 
         const email = document.getElementById("email").value
         const username = document.getElementById("username").value;
@@ -37,7 +47,7 @@ export default function RegisterPage() {
             return toast.error("Please fill up all the fields!")
         }
 
-       const rsp = await fetch(urls.backend+"/api/auth/register", {
+       const rsp = await fetch(urls.backend+`/api/auth/register?r_key=${captcha_token}`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
@@ -53,6 +63,7 @@ export default function RegisterPage() {
         const data = await rsp.json()
 
         if(!data.success) {
+            captchaRef.current.reset();
             unbuffer(document.getElementById("registerbtn"), "Register")
             return toast.error(data.message)
         } else {
@@ -105,7 +116,7 @@ export default function RegisterPage() {
 <div id="alertsection">
         <h4 className="text-muted">Register a new account.</h4>
         </div>
-          
+          <form onSubmit={register}>
             <div className="input-group mb-4 p-1">
             <span className="input-group-text" id="b2"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-at" viewBox="0 0 16 16">
   <path d="M13.106 7.222c0-2.967-2.249-5.032-5.482-5.032-3.35 0-5.646 2.318-5.646 5.702 0 3.493 2.235 5.708 5.762 5.708.862 0 1.689-.123 2.304-.335v-.862c-.43.199-1.354.328-2.29.328-2.926 0-4.813-1.88-4.813-4.798 0-2.844 1.921-4.881 4.594-4.881 2.735 0 4.608 1.688 4.608 4.156 0 1.682-.554 2.769-1.416 2.769-.492 0-.772-.28-.772-.76V5.206H8.923v.834h-.11c-.266-.595-.881-.964-1.6-.964-1.4 0-2.378 1.162-2.378 2.823 0 1.737.957 2.906 2.379 2.906.8 0 1.415-.39 1.709-1.087h.11c.081.67.703 1.148 1.503 1.148 1.572 0 2.57-1.415 2.57-3.643zm-7.177.704c0-1.197.54-1.907 1.456-1.907.93 0 1.524.738 1.524 1.907S8.308 9.84 7.371 9.84c-.895 0-1.442-.725-1.442-1.914z"/>
@@ -142,9 +153,20 @@ export default function RegisterPage() {
            
 
             </div>
+            <RECAPTCHA
+sitekey={"6LcYoQojAAAAADYeEE1m7AQFQAgix5lP-g5rPehU"}
+ref={captchaRef}
+render="explicit"
+required
+/>
+<p className="text-danger" id="bot-fail" style={{"marginBottom": "0%"}}></p>
+            <button className=" btn btn-success w-100 text-center" id="registerbtn" style={{"marginBottom": "2%", marginTop: "2%"}} >Register</button>   
+            </form>   
+            <div className="text-center">
+            <p  className="text-center text-muted " >Already have an account? <Link to="/login" className="text-success text-decoration-none">Login!</Link></p>
+           
             
-            <button className=" btn btn-success w-100 text-center" id="registerbtn" style={{"marginBottom": "2%", }} onClick={register}>Register</button>      
-            <Link to="/login" className="float-start text-muted">Already Have an Account? Login!</Link>
+            </div>
         </div>
         </div>
         </div>
